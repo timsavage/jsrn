cd714500a00243835bf3e4c534380381153b634d
Tim Savage <tim.savage@poweredbypenguins.org>
2013-06-05 00:50:44 +1000

    Basic "tests"

--- jsrn/tests.py
+++ jsrn/tests.py
@@ -3,9 +3,35 @@ import jsrn
 
 
 class Test(jsrn.Resource):
+    name = jsrn.StringField()
+
+
+class StyleField(jsrn.ObjectField):
     pass
 
 
-test = Test()
-print test._meta.resource_name
+class BaseElement(jsrn.Resource):
+    name = jsrn.StringField()
+    x = jsrn.IntegerField()
+    y = jsrn.IntegerField()
+    width = jsrn.IntegerField(required=False)
+    height = jsrn.IntegerField(required=False)
+    z_index = jsrn.IntegerField(required=False)
+    style = StyleField()
+
+    class Meta:
+        abstract = True
+
+
+class Block(BaseElement):
+    text = jsrn.StringField()
+
+
+b = Block()
+b.text = "EEK"
+b.name = "123"
+
+print jsrn.dumps(b)
+
+print b._meta.resource_name
 
