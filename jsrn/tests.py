fbf2e775140c325bec846a7cc9e95dbd35d09f99
Tim Savage <tim.savage@poweredbypenguins.org>
2013-06-05 00:56:21 +1000

    Expanded tests to be all of the elements from slate

--- jsrn/tests.py
+++ jsrn/tests.py
@@ -27,11 +27,35 @@ class Block(BaseElement):
     text = jsrn.StringField()
 
 
+class Image(BaseElement):
+    src = jsrn.StringField()
+
+
+class Group(BaseElement):
+    items = jsrn.ArrayField()
+
+
+class Layer(jsrn.Resource):
+    css_class = jsrn.StringField()
+    items = jsrn.ArrayField()
+
+
+class Page(jsrn.Resource):
+    title = jsrn.StringField()
+    width = jsrn.IntegerField(default=768)
+    height = jsrn.IntegerField(default=1024)
+    style = StyleField()
+    css_includes = jsrn.ArrayField()
+    layers = jsrn.ArrayField()
+
+
 b = Block()
 b.text = "EEK"
 b.name = "123"
 
 print jsrn.dumps(b)
+
+b.full_clean()
 
 print b._meta.resource_name
 
