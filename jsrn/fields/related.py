abc64a14d7e6e81688bd14652b612438c261686b
Tim Savage <tim.savage@poweredbypenguins.org>
2013-06-10 21:18:24 +1000

    Remove double up on validation. Changes to decoding to use create_resource_from_dict method

--- jsrn/fields/related.py
+++ jsrn/fields/related.py
@@ -1,12 +1,11 @@
-from jsrn import exceptions, registration
-from jsrn.exceptions import ValidationError
-from jsrn.resources import Resource
+from jsrn import exceptions
+from jsrn.resources import create_resource_from_dict
 from jsrn.fields import Field
 
-__all__ = ('ObjectOfType', 'ArrayOfType',)
+__all__ = ('ObjectAs', 'ArrayOf',)
 
 
-class ObjectOfType(Field):
+class ObjectAs(Field):
     default_error_messages = {
         'invalid': u"Must be a object of type ``%r``.",
     }
@@ -15,10 +14,10 @@ class ObjectOfType(Field):
         try:
             of._meta
         except AttributeError:
-            raise TypeError("``%r`` is not a valid type for a related field.")
+            raise TypeError("``%r`` is not a valid type for a related field." % of)
         self.of = of
 
-        super(ObjectOfType, self).__init__(**kwargs)
+        super(ObjectAs, self).__init__(**kwargs)
 
     def to_python(self, value):
         if value is None:
@@ -26,82 +25,44 @@ class ObjectOfType(Field):
         if isinstance(value, self.of):
             return value
         if isinstance(value, dict):
-            return self.of(**value)
-
+            return create_resource_from_dict(value, self.of._meta.resource_name)
         msg = self.error_messages['invalid'] % self.of
         raise exceptions.ValidationError(msg)
 
-    def clean(self, value, model_instance=None):
+    def clean(self, value):
         """
         Convert the value's type and run validation. Validation errors
         from to_python and validate are propagated. The correct value is
         returned if no error is raised.
         """
         value = self.to_python(value)
-        if value is not None:
-            value.full_clean()
         return value
 
-    def _deserialize(self, value):
-        if isinstance(value, dict):
-            resource_name = value.pop("$", None)
-            specified_resource_name = self.of._meta.resource_name
-            if resource_name != specified_resource_name:
-                if not self.of._meta.abstract:
-                    raise ValidationError("Invalid resource type, expected `%s` got `%s`" % (
-                        specified_resource_name, resource_name))
-                else:
-                    # TODO: validate that the specified resource is actually inherited off abstract
-                    resource_type = registration.get_resource(resource_name)
-                    if not resource_type:
-                        raise ValidationError("Unknown resource `%s`" % resource_name)
-            else:
-                resource_type = self.of
-            new_obj = resource_type()
-            new_obj.__setstate__(value)
-            return new_obj
-        return value
 
-    def value_for_object(self, obj, value):
-        """
-        Assign a value to an object
-        """
-        setattr(obj, self.attname, self._deserialize(value))
-
-
-class ArrayOfType(ObjectOfType):
+class ArrayOf(ObjectAs):
     default_error_messages = {
         'invalid': u"Must be a list of ``%r`` objects.",
     }
 
     def __init__(self, of, **kwargs):
-        kwargs.setdefault('default', list())
-        super(ArrayOfType, self).__init__(of, **kwargs)
+        kwargs.setdefault('default', list)
+        super(ArrayOf, self).__init__(of, **kwargs)
 
     def to_python(self, value):
         if value is None:
-            return None
+            return []
         if isinstance(value, list):
-            super_to_python = super(ArrayOfType, self).to_python
+            super_to_python = super(ArrayOf, self).to_python
             return [super_to_python(i) for i in value]
-
         msg = self.error_messages['invalid'] % self.of
         raise exceptions.ValidationError(msg)
 
-    def clean(self, value, model_instance=None):
+    def clean(self, value):
         """
         Convert the value's type and run validation. Validation errors
         from to_python and validate are propagated. The correct value is
         returned if no error is raised.
         """
         value = self.to_python(value)
-        if value is not None:
-            for item in value:
-                item.full_clean()
         return value
 
-    def value_for_object(self, obj, value):
-        """
-        De-serialize value into object.
-        """
-        setattr(obj, self.attname, [self._deserialize(i) for i in value])
