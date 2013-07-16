fd1349d88ad55744b7b66a07a517922498a6ed95
Tim Savage <tim.savage@poweredbypenguins.org>
2013-07-17 00:16:05 +1000

    Made compatible with Python3

--- src/jsrn/csv_parse.py
+++ src/jsrn/csv_parse.py
@@ -11,6 +11,12 @@ class ResourceReader(csv.DictReader):
         self.resources = resources
         csv.DictReader.__init__(self, f, *args, **kwargs)
 
+    # Python 2.6
     def next(self):
         d = csv.DictReader.next(self)
+        return create_resource_from_dict(d, self.resources._meta.resource_name)
+
+    # Python 3
+    def __next__(self):
+        d = csv.DictReader.__next__(self)
         return create_resource_from_dict(d, self.resources._meta.resource_name)
